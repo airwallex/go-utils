0901f44496ab4112b6375b89d52685cb68ed76d3
Xin Hao <haoxinst@gmail.com>
2022-06-29 16:26:23 +0800

    chore

--- url.go
+++ url.go
@@ -2,8 +2,8 @@ package utils
 
 import "net/url"
 
-func EncodeURL(s string) (string, error) {
-	parsedURL, err := url.Parse(s)
+func EncodeURL(rawURL string) (string, error) {
+	parsedURL, err := url.Parse(rawURL)
 	if err != nil {
 		return "", err
 	}
